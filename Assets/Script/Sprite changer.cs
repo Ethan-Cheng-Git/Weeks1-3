8d5830d8c9f24c3c37181493d0f4f69ed5f5edb8
Ethan Cheng <chenget@sheridancollege.ca>
2026-01-21 10:31:05 -0500

    Picked a random sprite using an array

--- Assets/Script/Sprite changer.cs
+++ Assets/Script/Sprite changer.cs
@@ -5,21 +5,30 @@ public class Spritechanger : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Color col;
+    public Sprite[] barrels;
+
+    public int randomNumber;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //spriteRenderer.color = Color.red; to set object colour to red
         //PickARandomColour();
+        pickARandomSprite();
+        //spriteRenderer.sprite = mySprite;   
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Keyboard.current.anyKey.wasPressedThisFrame == true)
-        //{
-        //    //PickARandomColour();
-        //}
+
+
+        if (Keyboard.current.anyKey.wasPressedThisFrame == true)
+        {
+            //PickARandomColour();
+            pickARandomSprite();
+        }
         // NOT THIS ONE!!! spriteRenderer. sprite .bounds.Contains(mousePos), this is at (0, 0)
         // use this one: spriteRenderer.bounds.Contains(mousePos) it's at the right position
         //get the mouse position
@@ -42,6 +51,16 @@ public class Spritechanger : MonoBehaviour
     void PickARandomColour()
     {
         spriteRenderer.color = Random.ColorHSV();
+    }
+    
+    void pickARandomSprite()
+    {
+        //get a random number between 0 and 2
+        randomNumber = Random.Range(0, barrels.Length);
+        //use it to set the sprite 
+       
+            spriteRenderer.sprite = barrels[randomNumber];
+        
     }
 
 }
