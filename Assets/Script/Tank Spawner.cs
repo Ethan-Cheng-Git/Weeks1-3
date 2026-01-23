0b77f02890a41a04dfebfe9032915451682e567f
DESKTOP-LT61UHN\camera <chenget@sheridancollege.ca>
2026-01-23 01:24:57 -0500

    made tanks increase in speed as more tanks was clicked

--- Assets/Script/Tank Spawner.cs
+++ Assets/Script/Tank Spawner.cs
@@ -4,6 +4,10 @@ using UnityEngine.InputSystem;
 public class TankSpawner : MonoBehaviour
 {
     public GameObject tankPrefab;
+    public int howManyTanks = 0;
+    public FirstScript tankScript;
+    public GameObject spawnedTank;
+    public SpriteRenderer tankSR;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,7 +21,31 @@ public class TankSpawner : MonoBehaviour
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
             //spawn the Prefab
-            Instantiate(tankPrefab);
+            // Instantiate a prefab: make this one appear
+            // Intanttiate a prefab, a Vector2 or Vector3, a Quaternion: make itappear at this position and rotation
+            //Instantiate(tankPrefab, transform.position, transform.rotation);
+
+            Vector2 spawnPos = Random.insideUnitCircle * 3;
+            //Quaternion.identity means no rotation
+            spawnedTank = Instantiate(tankPrefab, spawnPos, Quaternion.identity);
+            tankScript = spawnedTank.GetComponent<FirstScript>();
+            tankSR = spawnedTank.GetComponent<SpriteRenderer>();
+
+            howManyTanks += 1;
+
+            tankScript.speed = howManyTanks;
+       
+            //Instantiate a prefab, a Transform: makes it appear at 0, 0, as the child of that transformation
+
+        }
+        if (Mouse.current.rightButton.wasPressedThisFrame)
+        {
+            
+            Instantiate(tankPrefab, transform);
+
+
+            //Instantiate a prefab, a Transform: makes it appear at 0, 0, as the child of that transformation
+
         }
     }
 }
